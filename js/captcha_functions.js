5a015f214c187e4bbbc3ca9947a716849b8cbb6e
Seth Leedy <code@sethleedy.name>
2018-02-25 21:26:55 -0500

    incement of code
    Transfering saved staged files to another dev CP

--- js/captcha_functions.js
+++ js/captcha_functions.js
@@ -4,163 +4,230 @@
 
 class captchaClass {
 
-    // Runs on creation of object.
-    // 1] Pass the DOM id to attach the Captcha to.
-    // 2] Pass the id of the form to attach to its onSubmit event. Needed to validate the captcha on submission.
-    constructor(idOfCaptcha, idOfForm, idOfLog, source = "local") {
+	// Runs on creation of object.
+	// 1] Pass the DOM id to attach the Captcha to.
+	// 2] Pass the id of the form to attach to its onSubmit event. Needed to validate the captcha on submission.
+	constructor(idOfCaptcha, idOfForm, idOfLog, source = "local") {
 
-        // Setup first captcha code on run
-        this.idOfCaptcha = idOfCaptcha;
-        this.idOfCaptcha2 = document.getElementById(this.idOfCaptcha.id + "2"); // Set the text input id. It is hard-coded to whatever the captcha id is + "2".
-        this.idOfCaptcha3 = document.getElementById(this.idOfCaptcha.id + "3"); // The Reset button
+		// Setup first captcha code on run
+		this.idOfCaptcha = idOfCaptcha;
+		this.idOfCaptcha2 = document.getElementById(this.idOfCaptcha.id + "2"); // Set the text input id. It is hard-coded to whatever the captcha id is + "2".
+		this.idOfCaptcha3 = document.getElementById(this.idOfCaptcha.id + "3"); // The Reset button
 
-        // Create the captcha and set it to the above ID's
-        this.setCaptcha(source);
+		// Create the captcha and set it to the above ID's
+		this.setCaptcha(source);
 
-        // Bind the onsubmit event on the passed form to the validate method
-        this.formId = idOfForm;
-        if (document.addEventListener) { // avoid errors in incapable browsers. IE8 and under.
-            this.formId.addEventListener("submit", this.validateForm.bind(this));
-        } else {
-            console.log("'addEventListener' method not found. Incapable browser ?")
-        }
+		// Bind the onsubmit event on the passed form to the validate method
+		// For source = "local"
+		this.formId = idOfForm;
+		if (document.addEventListener) { // avoid errors in incapable browsers. IE8 and under.
+			if (source == "local") {
+				this.formId.addEventListener("submit", this.validateForm_local.bind(this));
+			}
+		} else {
+			console.log("'addEventListener' method not found. Incapable browser ?")
+		}
 
-        // Bind the log for spitting out messages
-        this.idOfLog = idOfLog;
+		// Bind the onsubmit event on the passed form to the validate method
+		// For source = "php"
+		this.formId = idOfForm;
+		if (document.addEventListener) { // avoid errors in incapable browsers. IE8 and under.
+			if (source == "php") {
+				this.formId.addEventListener("submit", this.validateForm_php.bind(this));
+			}
+		} else {
+			console.log("'addEventListener' method not found. Incapable browser ?")
+		}
+		
+		// Bind the log for spitting out messages
+		this.idOfLog = idOfLog;
 
-        // Bind the Reset button click event
-        if (document.addEventListener) { // avoid errors in incapable browsers. IE8 and under.
+		// Bind the Reset button click event
+		if (document.addEventListener) { // avoid errors in incapable browsers. IE8 and under.
 
-            this.idOfCaptcha3.addEventListener("click", function() {
-                let dummy = this.captchaCode(); // Getter call
-            }.bind(this)); // Watch the "this" context. "this" is not the class unless we bind it here. Normally it is the idOfCaptcha3 context since it is invoked from it.
-        }
-    }
+//			this.idOfCaptcha3.addEventListener("click", function() {
+//				let dummy = this.captchaCode(); // Getter call
+//			}.bind(this)); // Watch the "this" context. "this" is not the class unless we bind it here. Normally it is the idOfCaptcha3 context since it is invoked from it.
 
-    // This is what fires when requesting the code. It is read only so no one messes with the captcha in other JS code.
-    get captchaCode() {
+			this.idOfCaptcha3.addEventListener("click", this.getCaptchaCode.bind()); // Getter call
+			// Watch the "this" context. "this" is not the class unless we bind it here. Normally it is the idOfCaptcha3 context since it is invoked from it.
+		}
+	}
 
-        // Clear the log
-        this.appendLog();
+	// This is what fires when requesting the code.
+	// It is read only so no one messes with the captcha in other JS code spaces.
+	get captchaCode() {
+		
+		// redirect the Getter to this function as I could not bind an event to a getter.
+		this.getCaptchaCode();
+	}
 
-        // ReCall the method to create a new code.
-        // Return it, if called by another function.
-        return this.setCaptcha();
-    }
+	getCaptchaCode() {
 
-    validateForm(evt) {
+		// Clear the log
+		this.appendLog();
 
-        this.appendLog();
+		// ReCall the method to create a new code.
+		// Return it, if called by another function.
+		return this.setCaptcha();
+	}
 
-        let string1 = this.removeSpaces(this.idOfCaptcha.value);
-        let string2 = this.removeSpaces(this.idOfCaptcha2.value);
-        //alert(string1 + " " + string2);
+	validateForm_local(evt) {
 
-        if (string1 != string2 || string2 == "") {
+		this.appendLog();
 
-            // The following line will reset the code again. Comment out if you want them to reenter the same code after a mistake.
-            this.setCaptcha();
+		let string1 = this.removeSpaces(this.idOfCaptcha.value);
+		let string2 = this.removeSpaces(this.idOfCaptcha2.value);
+		//alert(string1 + " " + string2);
 
-            this.appendLog('Entered Invalid Captcha');
+		if (string1 != string2 || string2 == "") {
 
-            evt.preventDefault();
+			// The following line will reset the code again. Comment out if you want them to reenter the same code after a mistake.
+			this.setCaptcha();
 
-        } // else {
-        //
-        //			alert("CORRECT");
-        //		}
-    }
+			this.appendLog('Entered Invalid Captcha');
 
-    setCaptcha(source = "local") { // php or js or local
+			evt.preventDefault();
 
-        // Assign some temp variable of the Captcha Id.
-        let idOfCaptcha = this.idOfCaptcha;
-        let code = "";
+		} // else {
+		//
+		//			alert("CORRECT");
+		//		}
+	}
+	
+	validateForm_php(evt) {
 
-        // Increase the length of characters here.
-        let alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+		this.appendLog();
 
-        // If the source is local, create the captcha in browser.
-        // Else get it from a .PHP or NodeJS file
-        if (source == "local") {
-            let i;
-            for (i = 0; i < 6; i++) {
-                var a = alpha[Math.floor(Math.random() * alpha.length)];
-                var b = alpha[Math.floor(Math.random() * alpha.length)];
-                var c = alpha[Math.floor(Math.random() * alpha.length)];
-                var d = alpha[Math.floor(Math.random() * alpha.length)];
-                var e = alpha[Math.floor(Math.random() * alpha.length)];
-                var f = alpha[Math.floor(Math.random() * alpha.length)];
-                var g = alpha[Math.floor(Math.random() * alpha.length)];
-            }
-            let code = a + ' ' + b + ' ' + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g;
+		let string1 = this.removeSpaces(this.idOfCaptcha.value);
+		let string2 = this.removeSpaces(this.idOfCaptcha2.value);
+		// Pass some settings on creation of the fetch object.
+		let myInit = {
+			credentials: 'same-origin'
+		  };
+		
+		// Check if the local strings were tampered with.
+		if (string1 == string2) {
 
-            // Attach to DOM
-            idOfCaptcha.value = code;
-        }
+			// Return correct or not, then preventDefault or allow it to pass.
+			// Pass the inputted string and the session ID to PHP for checking if it is correct.
+			fetch("captcha.php?validate=" + string2, myInit).then(function(response) {
 
-        if (source == "php") {
-        	console.log(source);
-            // Setup AJAX request
-            var request = new XMLHttpRequest();
+				if (response.ok) {
+				
+					// Were good. Allow the form submission through.
+					console.log("Allow form submission!");
 
-            // Fill in variable
-            request.onreadystatechange = function() {
-                if (request.readyState === 4) {
-                	console.log("ready state = 4");
-                    var tmpcss = idOfCaptcha.style.border;
-                    idOfCaptcha.style.border = '1px dashed blue';
+					return response.blob();		
+				}
+				throw new Error('Network response was not ok.');
+			
+			}).then(response => response.text()).catch(function(error) {
+				console.log('There has been a problem with the fetch operation: ', error.message);
+			
+			});
+		} else if (string1 != string2 || string2 == "") {
+			
+			// The following line will reset the code again. Comment out if you want them to reenter the same code after a mistake.
+			this.setCaptcha();
 
-                    if (request.status === 200) {
-                    	console.log("request status = 200");
-                        idOfCaptcha.style.border = tmpcss;
+			this.appendLog('Entered Invalid Captcha');
+
+			evt.preventDefault();
+		}
+	}
+	
+	setCaptcha(source = "local") { // php or js or local
+
+		// Assign some temp variable of the Captcha Id.
+		let idOfCaptcha = this.idOfCaptcha;
+		let code = "";
+
+		// Increase the length of characters here.
+		let alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+
+		// If the source is local, create the captcha in browser.
+		// Else get it from a .PHP or NodeJS file
+		if (source == "local") {
+			let i;
+			for (i = 0; i < 6; i++) {
+				var a = alpha[Math.floor(Math.random() * alpha.length)];
+				var b = alpha[Math.floor(Math.random() * alpha.length)];
+				var c = alpha[Math.floor(Math.random() * alpha.length)];
+				var d = alpha[Math.floor(Math.random() * alpha.length)];
+				var e = alpha[Math.floor(Math.random() * alpha.length)];
+				var f = alpha[Math.floor(Math.random() * alpha.length)];
+				var g = alpha[Math.floor(Math.random() * alpha.length)];
+			}
+			let code = a + ' ' + b + ' ' + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g;
+
+			// Attach to DOM
+			idOfCaptcha.value = code;
+		}
+
+		if (source == "php") {
+			console.log(source);
+			// Setup AJAX request
+			var request = new XMLHttpRequest();
+
+			// Fill in variable
+			request.onreadystatechange = function() {
+				if (request.readyState === 4) {
+					console.log("ready state = 4");
+					var tmpcss = idOfCaptcha.style.border;
+					idOfCaptcha.style.border = '1px dashed blue';
+
+					if (request.status === 200) {
+						console.log("request status = 200");
+						idOfCaptcha.style.border = tmpcss;
 
 						// Attach to DOM
 						code = request.responseText;
-                        idOfCaptcha.value = code;
+						idOfCaptcha.value = code;
 
-                    } else {
-                        console.log('An error occurred during your request: ' + request.status + ' ' + request.statusText);
-                    }
-                }
-            }
+					} else {
+						console.log('An error occurred during your request: ' + request.status + ' ' + request.statusText);
+					}
+				}
+			}
 
-            request.open('GET', 'php/captcha.php');
-            request.send();
-        }
+			request.open('GET', 'php/captcha.php');
+			request.send();
+		}
 
-        if (source == "nodejs") {
+		if (source == "nodejs") {
 
-        }
+		}
 
-        // Set Colors of code
-        let colors = ["#B40404", "#beb1dd", "#b200ff", "#faff00", "#0000FF", "#FE2E9A", "#FF0080", "#2EFE2E"];
-        let rand = Math.floor(Math.random() * colors.length);
-        idOfCaptcha.style.color = colors[rand];
+		// Set Colors of code
+		let colors = ["#B40404", "#beb1dd", "#b200ff", "#faff00", "#0000FF", "#FE2E9A", "#FF0080", "#2EFE2E"];
+		let rand = Math.floor(Math.random() * colors.length);
+		idOfCaptcha.style.color = colors[rand];
 
-        // Return the code
-        return code;
-    }
+		// Return the code
+		return code;
+	}
 
-    // Internal use in the validateForm method.
-    removeSpaces(string) {
-        return string.split(' ').join('');
-    }
+	// Internal use in the validateForm method.
+	removeSpaces(string) {
+		return string.split(' ').join('');
+	}
 
-    appendLog(logString = "") {
-        if (logString != "") {
-            // Append to the log
-            this.idOfLog.innerHTML += logString;
-            // Reveal the div
-            this.idOfLog.style.display = "block";
-        } else {
-            // Empty the text
-            this.idOfLog.innerHTML = "";
-            // Hide the div
-            this.idOfLog.style.display = "none";
-        }
+	appendLog(logString = "") {
 
-        return;
-    }
+		if (logString != "") {
+			// Append to the log
+			this.idOfLog.innerHTML += logString;
+			// Reveal the div
+			this.idOfLog.style.display = "block";
+		} else {
+			// Empty the text
+			this.idOfLog.innerHTML = "";
+			// Hide the div
+			this.idOfLog.style.display = "none";
+		}
+
+		return;
+	}
 
 }
--- php/captcha.php
+++ php/captcha.php
@@ -7,8 +7,8 @@
 // Set session for all generated strings so we can check if the input was correct later.
 
 	$seed = str_split('abcdefghijklmnopqrstuvwxyz'
-	                 .'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	                 .'0123456789'); // and any other characters
+					 .'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+					 .'0123456789'); // and any other characters
 	shuffle($seed); // probably optional since array_is randomized; this may be redundant
 	$rand = ' ';
 
